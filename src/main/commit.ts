0f20b5e640ffca6fc1e49c94aa2922b08b990029
chlbri <bri_lvi@icloud.com>
2021-11-24 18:47:23 +0000

    fix commit

--- src/main/commit.ts
+++ src/main/commit.ts
@@ -5,7 +5,7 @@ import { log } from 'core';
 import { __commit } from '../cli/__commit';
 
 async function commit() {
-  return __commit().then(answers => {
+  __commit().then(answers => {
     log('answers', answers);
   });
 }
