42b1825d508524d56a8e4155b0bd02a8b0528d74
chlbri <bri_lvi@icloud.com>
2021-11-24 02:55:02 +0000

    ok

--- bin/cli/commit.js
+++ bin/cli/commit.js
@@ -4,16 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.__commit = void 0;
-const arg_1 = __importDefault(require("arg"));
 const inquirer_1 = __importDefault(require("inquirer"));
 const shelljs_1 = require("shelljs");
 const git_1 = require("../functions/git");
 const objects_1 = require("../schemas/objects");
 async function __commit(path) {
     const questions = [];
-    const args = (0, arg_1.default)({
-        '--path': String,
-    });
+    // const args = arg({
+    //   '--path': String,
+    // });
     const _isCommitted = (0, git_1.isCommitted)(path);
     _isCommitted;
     if (!_isCommitted) {
